ea0805d96e89fab6a1d782b9b2a826cf5dea4872
trifpap <50261221+trifpap@users.noreply.github.com>
2022-10-16 21:46:33 +0300

    Update streamlit_hello_app.py

--- streamlit_hello_app.py
+++ streamlit_hello_app.py
@@ -5,7 +5,15 @@ import pandas as pd
 st.title('Hello World!')
 st.balloons()
 
+# highlight some elements in a table
+st.title('Highlight some elements in a interactive table')
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+# display data points on a map
 st.title('Display data points on a map')
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
