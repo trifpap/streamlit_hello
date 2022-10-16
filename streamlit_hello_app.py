2dcf8a6a0847725713d2c3257d34796b64b06f5b
trifpap <50261221+trifpap@users.noreply.github.com>
2022-10-16 21:42:33 +0300

    Update streamlit_hello_app.py

--- streamlit_hello_app.py
+++ streamlit_hello_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title('Hello World!')
 st.balloons()
+
+
+st.title('Display data points on a map')
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
