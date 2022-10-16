6eab0ffb3b1d4dea449fa551142f1b030fe9a03a
trifpap <50261221+trifpap@users.noreply.github.com>
2022-10-16 22:04:01 +0300

    Update streamlit_hello_app.py

--- streamlit_hello_app.py
+++ streamlit_hello_app.py
@@ -20,3 +20,15 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+# choose from a series
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
